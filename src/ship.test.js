7cffebca7d47fbedf9baac1e400fa71ef65e59d3
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-21 15:27:02 +0800

    fix ship test

--- src/ship.test.js
+++ src/ship.test.js
@@ -8,11 +8,11 @@ describe("ship test", () => {
   });
 
   test("not sunk after one hits", () => {
+    ship.hit();
     expect(ship.isSunk()).toBe(false);
   });
 
   test("Sunk after 3 hits", () => {
-    ship.hit();
     ship.hit();
     ship.hit();
 
