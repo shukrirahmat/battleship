6b544273a7afbe65aaf739bea71e93642383b993
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-30 15:34:56 +0800

    add smarter ai

--- src/player.js
+++ src/player.js
@@ -1,17 +1,17 @@
 import Gameboard from "./gameboard.js";
 
-function Player(boardsize, name="Player") {
+function Player(boardsize, name = "Player") {
   let board = Gameboard(boardsize);
   const getBoard = () => board;
   const getName = () => name;
   const resetBoard = () => {
     board = Gameboard(boardsize);
-  }
+  };
 
   return {
     getName,
     getBoard,
-    resetBoard
+    resetBoard,
   };
 }
 
@@ -19,32 +19,45 @@ function computerController(computer) {
   let size = computer.getBoard().getSize();
   let possibleTarget = setUpPossibleTarget();
 
-  function choose() {
-    if (possibleTarget.length === 0) return null;
+  function choose(lastGridHit = null) {
+    let choice = null;
+    let index;
 
-    const index = Math.floor(Math.random() * possibleTarget.length);
-    const choice = possibleTarget[index];
+    if (possibleTarget.length === 0) return null;
+    if (lastGridHit) {
+      possibleTarget.forEach((target, i) => {
+        if (target[0] === lastGridHit[0] || target[1] === lastGridHit[1]) {
+          choice = target;
+          index = i;
+        }
+      });
+    }
+
+    if (choice === null) {
+      index = Math.floor(Math.random() * possibleTarget.length);
+      choice = possibleTarget[index];
+    }
     possibleTarget.splice(index, 1);
     return choice;
   }
 
   function setUpPossibleTarget() {
-    let list = []
+    let list = [];
 
     for (let x = 0; x < size; x++) {
       for (let y = 0; y < size; y++) {
-        list.push([x,y]);
+        list.push([x, y]);
       }
     }
     return list;
   }
 
   return {
-    choose
+    choose,
   };
 }
 
-function ComputerPlayer(boardsize, name="Computer") {
+function ComputerPlayer(boardsize, name = "Computer") {
   const computer = Player(boardsize, name);
 
   return {
